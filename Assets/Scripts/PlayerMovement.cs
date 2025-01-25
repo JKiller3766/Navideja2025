cf1e265e99c70ea2df62af8adddfbe2486eac8ba
LucaCalveiro <133456804+LucaCalveiro@users.noreply.github.com>
2025-01-25 23:24:58 +0100

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float velocidad = 5f;
+    private float velocidad = 2f;
     public GameObject huevoPrincipal;
     private int contadorLoopSprite = 1;
     private Boolean subiendoContador = true;
     [SerializeField]
     private SpriteRenderer _renderer;
+    int i = 0;
 
     public void Start()
     {
@@ -21,24 +23,17 @@ public class PlayerMovement : MonoBehaviour
 
     public void Update()
     {
-        float velocidadX = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
-        float velocidadY = Input.GetAxisRaw("Vertical") * Time.deltaTime;
-        
+
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
         Vector3 posicion = transform.position;
-        CambioDeSprite(Input.GetAxisRaw("Horizontal") * Time.deltaTime, Input.GetAxisRaw("Vertical") * Time.deltaTime);
 
-        transform.position = transform.position + new Vector3(velocidadX, velocidadY, 0) * velocidad * Time.deltaTime;
+        transform.position = posicion + new Vector3(inputX * Time.fixedDeltaTime, inputY * Time.fixedDeltaTime, 0);
 
-
+        CambioDeSprite(inputX, inputY);
+        i++;
     }
-
-    /*public void FixedUpdate()
-    {
-        if (timeCounter % 5 == 0
-            ) {
-            CambioDeSprite(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        }
-    }*/
 
     Sprite Load(string imageName, string spriteName)
     {
@@ -66,21 +61,18 @@ public class PlayerMovement : MonoBehaviour
             switch (contadorLoopSprite)
             {
                 case 1:
-                    _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_0"); 
-                    Debug.Log("1");
+                    _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_0");
                     break;
                 case 2:
-                    _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_1"); 
-                    Debug.Log("2");
+                    _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_1");
                     break;
                 case 3:
-                    _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_2"); 
-                    Debug.Log("3");
+                    _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_2");
                     break;
                 case 4:
-                    _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_3"); 
-                    Debug.Log("4");
+                    _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_3");
                     break;
+
             }
         }
         else if (posX > 0 && posY > 0)
@@ -89,19 +81,15 @@ public class PlayerMovement : MonoBehaviour
             {
                 case 1:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_4");
-                    Debug.Log("5");
                     break;
                 case 2:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_5");
-                    Debug.Log("6");
                     break;
                 case 3:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_6");
-                    Debug.Log("7");
                     break;
                 case 4:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_7");
-                    Debug.Log("8");
                     break;
 
             }
@@ -112,19 +100,15 @@ public class PlayerMovement : MonoBehaviour
             {
                 case 1:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_8");
-                    Debug.Log("9");
                     break;
                 case 2:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_9");
-                    Debug.Log("10");
                     break;
                 case 3:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_10");
-                    Debug.Log("11");
                     break;
                 case 4:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_11");
-                    Debug.Log("12");
                     break;
 
             }
@@ -135,19 +119,15 @@ public class PlayerMovement : MonoBehaviour
             {
                 case 1:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_12");
-                    Debug.Log("13");
                     break;
                 case 2:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_13");
-                    Debug.Log("14");
                     break;
                 case 3:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_14");
-                    Debug.Log("15");
                     break;
                 case 4:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_15");
-                    Debug.Log("16");
                     break;
 
             }
@@ -158,19 +138,15 @@ public class PlayerMovement : MonoBehaviour
             {
                 case 1:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_16");
-                    Debug.Log("17");
                     break;
                 case 2:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_17");
-                    Debug.Log("18");
                     break;
                 case 3:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_18");
-                    Debug.Log("19");
                     break;
                 case 4:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_19");
-                    Debug.Log("20");
                     break;
 
             }
@@ -181,19 +157,15 @@ public class PlayerMovement : MonoBehaviour
             {
                 case 1:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_20");
-                    Debug.Log("21");
                     break;
                 case 2:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_21");
-                    Debug.Log("22");
                     break;
                 case 3:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_22");
-                    Debug.Log("23");
                     break;
                 case 4:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_23");
-                    Debug.Log("24");
                     break;
 
             }
@@ -204,19 +176,15 @@ public class PlayerMovement : MonoBehaviour
             {
                 case 1:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_24");
-                    Debug.Log("25");
                     break;
                 case 2:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_25");
-                    Debug.Log("26");
                     break;
                 case 3:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_26");
-                    Debug.Log("27");
                     break;
                 case 4:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_27");
-                    Debug.Log("28");
                     break;
 
             }
@@ -227,19 +195,15 @@ public class PlayerMovement : MonoBehaviour
             {
                 case 1:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_28");
-                    Debug.Log("29");
                     break;
                 case 2:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_29");
-                    Debug.Log("30");
                     break;
                 case 3:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_30");
-                    Debug.Log("31");
                     break;
                 case 4:
                     _renderer.sprite = Load("Sprites/huevosprite", "huevosprite_31");
-                    Debug.Log("32");
                     break;
 
             }

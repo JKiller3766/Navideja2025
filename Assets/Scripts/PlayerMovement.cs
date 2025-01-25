2601624d5b9156fa402d746f7685663456509a77
LucaCalveiro <133456804+LucaCalveiro@users.noreply.github.com>
2025-01-25 23:30:17 +0100

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
         subiendoContador = true;
     }
 
-    public void Update()
+    public void Update() 
     {
 
         float inputX = Input.GetAxisRaw("Horizontal");

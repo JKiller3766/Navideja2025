61b951fd90f1aac69a73af83dc08e03d53e91da0
Vector-776 <polopimienta42@gmail.com>
2025-01-26 19:51:42 +0100

    dsasad

---
+++ Assets/Scripts/Nivel 2/Muerte_2.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Muerte_2 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+ 
+    private void OnCollisionEnter2D(Collision2D collision) 
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+}

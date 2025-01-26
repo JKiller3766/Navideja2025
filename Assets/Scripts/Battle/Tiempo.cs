9d8ed946623a2687126d4c0f0f3851e7cfcf2248
Vector-776 <polopimienta42@gmail.com>
2025-01-26 16:32:23 +0100

    cx c

--- Assets/Scripts/Battle/Tiempo.cs
+++ Assets/Scripts/Battle/Tiempo.cs
@@ -1,34 +1,26 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;  
-using UnityEngine.UIElements;
+using UnityEngine;
 using TMPro;
 
 public class Tiempo : MonoBehaviour
 { 
-    [SerializeField] int min, seg;
+    [SerializeField] int min;
+    [SerializeField] float seg;
     [SerializeField] TextMeshProUGUI tiempo;
-
-    private float restante;
-    private bool enMarcha;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created 
-    private void Awake() {
-        enMarcha = true;
-        restante = min*60 + seg;
-    }
+ 
     // Update is called once per frame
     void Update()
-    {
-        if (enMarcha)
-        {
-            restante = Time.deltaTime;
-            if(restante < 1) {
-                //Matar
+    { 
+        seg -= Time.deltaTime;
+        if(seg <= 0) {
+            if(min != 0) {
+                min--;
+                seg = 60;
             }
-            int tempMin = (int)(restante/60);
-            int tempSeg = (int)(restante % 60);
-            tiempo.text = string.Format("{00:00}:{01:00}", tempMin, tempSeg);
         }
+
+        tiempo.text = "" + min.ToString() + ":" + ((int)seg).ToString() ;
     }
+ 
 }

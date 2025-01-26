fdcbfc9b17941b2f5780ea3ed47696bc2c86d2df
David <43551883+ninjin780@users.noreply.github.com>
2025-01-26 19:20:08 +0100

    CambioEscenaCine-nivel1

--- Assets/Scripts/MenuInicial.cs
+++ Assets/Scripts/MenuInicial.cs
@@ -8,7 +8,7 @@ using UnityEngine.SceneManagement;
 public class MenuInicial : MonoBehaviour
 {
    public void Jugar(){
-        SceneManager.LoadScene(1); //escena 1 es el juego
+        SceneManager.LoadScene(1); //escena 1 es la cinematica
    }
 
    public void Salir() {
